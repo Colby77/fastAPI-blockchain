b61e626d4d87b5afd7845773625e804bcd6a5b80
Colby Workman <cgw2319@gmail.com>
2022-03-23 17:33:19 -0600

    docstrings added

--- blockchain.py
+++ blockchain.py
@@ -37,12 +37,26 @@ import json
 class Blockchain:
     
     def __init__(self):
+        """
+        Creates first block in blockchain (genesis block)
+        and starts chain
+        """
         self.chain = list()
         genesis_block = self.create_block(
             data='The first block', proof=1, previous_hash='0', index=1)
         self.chain.append(genesis_block)
 
     def mine_block(self, data: str):
+        """
+        Gets the previous block and proof
+
+        Creates a new proof using previous proof, new index and data
+
+        Hashes previous block and creates the new block with the previous 
+        hash, data, proof and index
+
+        Adds new block to the chain
+        """
         previous_block = self.get_previous_block()
         previous_proof = previous_block["proof"]
         index = len(self.chain) + 1
@@ -62,11 +76,21 @@ class Blockchain:
         return hashlib.sha256(encoded_block).hexdigest()
 
     def check_proof(self, new_proof: int, previous_proof: int, index: str, data: str):
+        """
+        Checks a proof using mathematical formula in 'check'
+        """
         check = str(new_proof ** 2 - previous_proof ** 2 + index) + data
 
         return check.encode()
 
     def proof_of_work(self, previous_proof: str, index: int, data: str):
+        """
+        Checks if the first 4 digits of a given hashed value is
+        equal to '0000'
+
+        Value starts at 1 and keeps going until the correct hashed value
+        is found
+        """
         new_proof = 1
         proof_checked = False
 
@@ -87,9 +111,12 @@ class Blockchain:
         return new_proof
 
     def get_previous_block(self):
+        """Returns last block in chain"""
         return self.chain[-1]
 
     def create_block(self, data: str, proof: int, previous_hash: str, index: int):
+        """Creates new block"""
+
         block = {
             "index": index,
             "timestamp" : str(dt.datetime.now()),
@@ -101,6 +128,18 @@ class Blockchain:
         return block
 
     def is_chain_valid(self):
+        """
+        Validates the block chain
+
+        Starts at genesis block and for each block
+        in chain, checks that:
+            The next block's previous hash
+            isn't the same as the current block's hash
+
+            A hashed value using the current proof
+            matches the hashed value of the next block in chain  
+        """
+
         current_block = self.chain[0]
         block_index = 1
 
